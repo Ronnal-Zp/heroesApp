8a7ced72ef7f9144b81f0a36512d1e365b2301fc
Aldahir Zamora <ronn.zp.3@gmail.com>
2024-08-09 11:58:18 -0500

    primeras rutas

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { NewComponent } from './pages/new/new.component';
+import { SearchComponent } from './pages/search/search.component';
+import { ListComponent } from './pages/list/list.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'new', component: NewComponent },
+      { path: 'search', component: SearchComponent },
+      { path: 'edit/:id', component: NewComponent },
+      { path: 'list', component: ListComponent },
+      { path: ':id', component: HeroeComponent },
+      { path: '**', redirectTo: 'list' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
