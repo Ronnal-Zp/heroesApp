41c2e171fa25fc4c1324d93cac85c8d7d1d76d84
Aldahir Zamora <ronn.zp.3@gmail.com>
2024-08-09 11:40:42 -0500

    re estructurar directorio modulo heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { LayoutComponent } from './heroes/layout/layout.component';
-import { ListComponent } from './heroes/list/list.component';
-import { NewComponent } from './heroes/new/new.component';
-import { SearchComponent } from './heroes/search/search.component';
+import { HeroeComponent } from './heroes/pages/heroe/heroe.component';
+import { LayoutComponent } from './heroes/pages/layout/layout.component';
+import { ListComponent } from './heroes/pages/list/list.component';
+import { NewComponent } from './heroes/pages/new/new.component';
+import { SearchComponent } from './heroes/pages/search/search.component';
 
 @NgModule({
   declarations: [
